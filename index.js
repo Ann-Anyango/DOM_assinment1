3ec39179c97c8dff73fed035c3e802225a30150c
Ann-Anyango <annmwangombe@gmail.com>
2023-05-24 16:25:48 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+document.getElementById('body').style.backgroundColor='silver';
+document.getElementById('title').style.color='green';
+
+let li = document.createElement('li');
+li.innerHTML = " Berry";
+document.getElementById('fruList').appendChild(li)
+
+
+document.getElementById('frut').style.textTransform="Uppercase";
+
+
+
+let lists = document.createElement('lists');
+lists.innerHTML =" Mchicha";
+document.getElementById('vegList').appendChild(lists)
+
+
+
+
+// let h1 = document.createElement('h1');
+//  h1.innerHTML="I am new here";
